0aee5eea0c36c6f4cd53f6e446d3dcca2b1333c0
MohanadArkia <arkia.ar.47@gmail.com>
2024-08-16 05:10:40 +0300

    Few changes to the styles

--- pages/reminders/index.tsx
+++ pages/reminders/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import styles from "../../src/styles/reminders.module.css";
+import Header from '@/components/Header';
+import Images from '@/assets/images/images';
+import BottomNavigator from '@/components/BottomNavigator';
 
 const index = () => {
   return (
-    <div>Reminders Screen</div>
+    <div className={styles.container}>
+        <Header title="Reminders" img={Images.LeftArrow()} style={styles.header}/>
+        <BottomNavigator />
+    </div>
   )
 }
 
