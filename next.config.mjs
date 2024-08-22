a4cd18f9f51ac57934c73225ea276668cfb09689
MohanadArkia <arkia.ar.47@gmail.com>
2024-08-22 13:01:43 +0300

    Installed a package that removes unused css on production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+// export default nextConfig;
 
-export default nextConfig;
+import withPurgeCSSModules from "next-purge-css-modules";
+
+export default withPurgeCSSModules({
+  purgeCss: {
+    content: [
+      "./pages/**/*.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
+    ],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    safelist: ["html", "body"],
+  },
+});
