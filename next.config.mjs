9300936c0ab57d15481507a4aca1978cb37044b7
MohanadArkia <arkia.ar.47@gmail.com>
2024-08-22 13:12:25 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,3 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-// export default nextConfig;
-
-import withPurgeCSSModules from "next-purge-css-modules";
-
-export default withPurgeCSSModules({
-  purgeCss: {
-    content: [
-      "./pages/**/*.{js,jsx,ts,tsx}",
-      "./components/**/*.{js,jsx,ts,tsx}",
-    ],
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    safelist: ["html", "body"],
-  },
-});
+const nextConfig = {};
+export default nextConfig;
